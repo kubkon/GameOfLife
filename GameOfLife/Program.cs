b76d82a8e92f81bcd7b6b115082a298360c09bac
Jakub Konka <kubkon@jakubkonka.com>
2015-03-12 20:08:38 +0000

    Initial commit.
    Tested CountLiveNeighbours and ApplyRules.

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class World
+    {
+        int rows;
+        int columns;
+        public int[,] grid;
+
+        public World(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            grid = new int[rows, columns];
+        }
+
+        void SetCellLive(int[] indices)
+        {
+            grid[indices[0], indices[1]] = 1;
+        }
+
+        void SetCellDead(int[] indices)
+        {
+            grid[indices[0], indices[1]] = 0;
+        }
+
+        public void Initialise(int[][] live)
+        {
+            for (int i = 0; i < live.GetLength(0); i++)
+            {
+                SetCellLive(live[i]);
+            }
+        }
+
+        public int CountLiveNeighbours(int x, int y)
+        {
+            // Get all neighbours
+            List<int> neighbours = new List<int>();
+            for (int i = -1; i < 2; i++)
+            {
+                int k = x + i;
+
+                for (int j = -1; j < 2; j++)
+                {
+                    int l = y + j;
+
+                    if (k >= 0 && k < rows && l >= 0 && l < columns && (k != x || l != y))
+                    {
+                        neighbours.Add(grid[k, l]);
+                    }
+                }
+            }
+            return neighbours.Sum();
+        }
+
+        public int ApplyRules(int x, int y, int currentCell, int liveNeighbours)
+        {
+            int result = 0;
+            if (currentCell > 0)
+            {
+                if (liveNeighbours < 2)
+                    result = 0;
+                else if (liveNeighbours == 2 || liveNeighbours == 3)
+                    result = 1;
+                else
+                    result = 0;
+            }
+            else
+            {
+                if (liveNeighbours == 3)
+                    result = 1;
+                else
+                    result = 0;
+            }
+            return result;
+        }
+
+        public void Evolve()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    int currentCell = grid[i, j];
+                    // Count live neighbours of the current cell
+                    int liveNeighbours = CountLiveNeighbours(i, j);
+                    // Apply rules
+                    int result = ApplyRules(i, j, currentCell, liveNeighbours);
+                    // Save result
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    string el = grid[i, j] == 1 ? "x" : ".";
+                    sb.Append(el);
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            World world = new World(10, 12);
+            int[][] liveCells = new int[][] {
+                new int[] {0, 1},
+                new int[] {1, 2},
+                new int[] {2, 0},
+                new int[] {2, 1},
+                new int[] {2, 2}
+            };
+            world.Initialise(liveCells);
+            world.Evolve();
+            Console.Write(world);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GameOfLifeTests/WorldUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameOfLife;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class WorldUnitTest
+    {
+        static World world;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            world = new World(2, 2);
+            world.Initialise(new int[][] {
+                new int[] {0, 0},
+                new int[] {1, 1}
+            });
+        }
+
+        [TestMethod]
+        public void TestCountLiveNeighbours()
+        {
+
+            int live = world.CountLiveNeighbours(0, 0);
+            Assert.AreEqual(1, live);
+            live = world.CountLiveNeighbours(0, 1);
+            Assert.AreEqual(2, live);
+            live = world.CountLiveNeighbours(1, 0);
+            Assert.AreEqual(2, live);
+            live = world.CountLiveNeighbours(1, 1);
+            Assert.AreEqual(1, live);
+        }
+
+        [TestMethod]
+        public void TestApplyRules()
+        {
+            int x = 0, y = 0;
+            int live = world.CountLiveNeighbours(x, y);
+            int future = world.ApplyRules(x, y, world.grid[x, y], live);
+            Assert.AreEqual(0, future);
+        }
+    }
+}
